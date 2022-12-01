8861ac4341bc08888adcfaf4dad739a6c4b8346f
Eder Henrique <ederhscc@gmail.com>
2022-11-30 21:05:13 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,18 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	//Apenas classes do mesmo pacote e subclasses poderão acessar.
+	protected Board getBoard() {
+		return board;
+	}
+	
+}
